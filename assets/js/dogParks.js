6a88c2c61a02b79324f2d69e7009645c893944de
kai <kai@kaiconsulting.co>
2021-11-30 20:42:42 -0600

    working with js file

--- assets/js/dogParks.js
+++ assets/js/dogParks.js
@@ -1,18 +1,18 @@
 // // Create a request variable and assign a new XMLHttpRequest object to it.
-var requestURL = "https://data.actmapi.act.gov.au/arcgis/rest/services/data_extract/Community_Facilities_and_Assets/MapServer/5/query?where=1%3D1&outFields=*&outSR=4326&f=json"
-var responseText = document.getElementById('response-text');
+// var requestURL = "https://data.actmapi.act.gov.au/arcgis/rest/services/data_extract/Community_Facilities_and_Assets/MapServer/5/query?where=1%3D1&outFields=*&outSR=4326&f=json"
+// var responseText = document.getElementById('response-text');
 
-function getApi(requestURL) {
-  fetch (requestUrl)
-    .then(function (response) {
-      console.log(response);
-      if (response.status === 200) {
-        let ids = response.json();
-        console.log(ids);
-          // return response.json();
-    });
-}
-getApi (requestURL);
+// function getApi(requestURL) {
+//   fetch (requestUrl)
+//     .then(function (response) {
+//       console.log(response);
+//       if (response.status === 200) {
+//         let ids = response.json();
+//         console.log(ids);
+//           // return response.json();
+//     });
+// }
+// getApi (requestURL);
 
 // var requestUrl = "https://data.actmapi.act.gov.au/arcgis/rest/services/data_extract/Community_Facilities_and_Assets/MapServer/5/query?where=1%3D1&outFields=*&outSR=4326&f=json"
 // var responseText = document.getElementById('response-text');
