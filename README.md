01005d773b87a8a7971cabd30b9627e0181572b1
kai <kai@kaiconsulting.co>
2021-11-30 21:31:24 -0500

    working with api

--- README.md
+++ README.md
@@ -1,6 +1,23 @@
 # project01
 Project 01
 
+
+## User Story:
+
+* As dog owner, I want to use the dog park finder to see dog-friendly parks near me.
+
+## Acceptance Criteria
+
+* It's done when the user clicks on the Dog Parks webpage and list of dog parks populates
+<!-- If user account has zipcode, can pull user zipcode to populate dog parks nearby -->
+<!-- otherwise, input zipcode (or use current location?) -->
+
+* It's done when 
+
+* It's done when 
+
+* It's done when 
+
 # Description
 
 # Project Members
--- assets/js/dogParks.js
+++ assets/js/dogParks.js
@@ -1,15 +1,33 @@
+// // Create a request variable and assign a new XMLHttpRequest object to it.
 var requestURL = "https://data.actmapi.act.gov.au/arcgis/rest/services/data_extract/Community_Facilities_and_Assets/MapServer/5/query?where=1%3D1&outFields=*&outSR=4326&f=json"
 var responseText = document.getElementById('response-text');
 
 function getApi(requestURL) {
   fetch (requestUrl)
-    .then(function (response)) {
+    .then(function (response) {
       console.log(response);
       if (response.status === 200) {
-        reponseText = response.json();
-        SVGComponentTransferFunctionElement.log(ids);
-          return response.json();
-      });
-    }
+        let ids = response.json();
+        console.log(ids);
+          // return response.json();
+    });
 }
 getApi (requestURL);
+
+// var requestUrl = "https://data.actmapi.act.gov.au/arcgis/rest/services/data_extract/Community_Facilities_and_Assets/MapServer/5/query?where=1%3D1&outFields=*&outSR=4326&f=json"
+// var responseText = document.getElementById('response-text');
+
+// function getApi(requestUrl) {
+//   fetch(requestUrl)
+//     .then(function (response) {
+//       console.log(response);
+//       if (response.status === 200) {
+//         responseText.textContent = response.status;
+//       }
+//         let ids = response.json();
+//         console.log(ids);
+//         // return response.json();
+//   });
+// }
+
+// getApi(requestUrl);
