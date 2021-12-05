7e49c06a6016287417e7e03483ff8d2b0c9f2005
Kirtley Adams <hello@kirtleymichelle.com>
2021-12-04 20:14:44 -0500

    added local storage

--- assets/js/breeds.js
+++ assets/js/breeds.js
@@ -231,32 +231,32 @@ function updatePage() {
 }
 
 //localStorage
-$('.saveBtn').click(function() {
-    if (localStorage.getItem('dataStorage')!== null){
-        dataStorage = JSON.parse(localStorage.getItem('dataStorage'));
+$('#fav').click(function() {
+    console.log("running")
+    if (localStorage.getItem('Breeds') === null){
+        localStorage.setItem('Breeds', JSON.stringify(dogBreed));
     }
-    let time = $(this).parent().children()[0].innerHTML;
-    console.log(time);
-
-    let storage = {times: time, text: text};
-    console.log(storage);
-    dataStorage.push(storage);
-    console.log(dataStorage);
-    localStorage.setItem("dataStorage", JSON.stringify(dataStorage));
 })
-let dataStorage = [];
-let storedData = JSON.parse(localStorage.getItem("dataStorage"));
-console.log(storedData);
-storedData.forEach(element => {
-    console.log(element)
-    let checkTime = element.times;
-    let checkMsg = element.text;
-    console.log(checkTime, checkMsg)
-    for (let i = 1; i < 10; i++) {
-        console.log('i', i)
-        let checkId = $(`#${i}`).parent().children()[0].innerHTML;
-        if (checkTime == checkId) {
-            $(`#${i}`).parent().children()[1].textContent = checkMsg;
-        }
-    }
-});
+
+//     let storage = {times: time, text: text};
+//     console.log(storage);
+//     dataStorage.push(storage);
+//     console.log(dataStorage);
+//     localStorage.setItem("dataStorage", JSON.stringify(dataStorage));
+// })
+// let dataStorage = [];
+// let storedData = JSON.parse(localStorage.getItem("dataStorage"));
+// console.log(storedData);
+// storedData.forEach(element => {
+//     console.log(element)
+//     let checkTime = element.times;
+//     let checkMsg = element.text;
+//     console.log(checkTime, checkMsg)
+//     for (let i = 1; i < 10; i++) {
+//         console.log('i', i)
+//         let checkId = $(`#${i}`).parent().children()[0].innerHTML;
+//         if (checkTime == checkId) {
+//             $(`#${i}`).parent().children()[1].textContent = checkMsg;
+//         }
+//     }
+// });
